7d1db7beff428613d085a2073ce92f0db9f050a5
cute gaf <157330073+kadupadu@users.noreply.github.com>
2024-08-19 20:55:40 +0530

    Create healthcheck.js

---
+++ healthcheck.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/health') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('OK');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+server.listen(8080, () => {
+    console.log('Health check server running on port 8080');
+});
